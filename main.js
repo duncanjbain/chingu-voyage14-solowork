d554a7c91d7fde1df2db0f44cf4d41e59e5de7d5
Duncan Bain <duncan@bain.io>
2019-11-26 20:55:09 +0000

    fixed example text to ease text reset implentation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
+let originalElements = document.getElementsByClassName('fonts-card-example');
 
 function updateFontSizeFromField(newFontSize) {
     for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
@@ -9,6 +10,7 @@ function updateFontSizeFromField(newFontSize) {
 function resetFontExample() {
     for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
         elementsToUpdateFontSize[i].style.fontSize = "40px";
+        elementsToUpdateFontSize[i].textContent = originalElements[i].textContent;
     }
 }
 
