37e0cbc1e6bf0c2ca6e3528e644646f038e32ca5
Duncan Bain <duncan@bain.io>
2019-11-26 20:46:49 +0000

    added ability to change example text content

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
+let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
+
 function updateFontSizeFromField(newFontSize) {
-    let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
     for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
         elementsToUpdateFontSize[i].style.fontSize = newFontSize.value + 'px';
     }
 }
 
 function resetFontExample() {
-    let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
     for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
         elementsToUpdateFontSize[i].style.fontSize = "40px";
+    }
+}
+
+function updateFontExampleText(newExampleText) {
+    for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
+        elementsToUpdateFontSize[i].textContent = newExampleText.value;
     }
 }
