74e132058a09d5a87baaaa40533df6cd228f7dcd
Duncan Bain <duncan@bain.io>
2019-11-26 22:36:54 +0000

    refactor variable name

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
+let customTextElements = document.getElementsByClassName('fonts-card-example');
 
 function updateFontSizeFromField(newFontSize) {
-    for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
-        elementsToUpdateFontSize[i].style.fontSize = newFontSize.value + 'px';
+    for (var i = 0; i < customTextElements.length; i++) {
+        customTextElements[i].style.fontSize = newFontSize.value + 'px';
     }
 }
 
 function resetFontExample() {
-    for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
-        elementsToUpdateFontSize[i].style.fontSize = "24px";
-        elementsToUpdateFontSize[i].textContent = "A shining crescent far beneath the flying vessel.";
+    for (var i = 0; i < customTextElements.length; i++) {
+        customTextElements[i].style.fontSize = "24px";
+        customTextElements[i].textContent = "A shining crescent far beneath the flying vessel.";
     }
 }
 
 function updateFontExampleText(newExampleText) {
     if (newExampleText.value === "") {
-        for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
-            elementsToUpdateFontSize[i].textContent = "A shining crescent far beneath the flying vessel.";
+        for (var i = 0; i < customTextElements.length; i++) {
+            customTextElements[i].textContent = "A shining crescent far beneath the flying vessel.";
         }
     }
     else {
-        for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
-            elementsToUpdateFontSize[i].textContent = newExampleText.value;
+        for (var i = 0; i < customTextElements.length; i++) {
+            customTextElements[i].textContent = newExampleText.value;
         }
     }
 }
