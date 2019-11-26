dfb219f06aed61fbb3b542829e0d68df30aca34d
Duncan Bain <duncan@bain.io>
2019-11-26 19:54:09 +0000

    added font resize feature, resets font size only

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function updateFontSizeFromField(newFontSize) {
         elementsToUpdateFontSize[i].style.fontSize = newFontSize.value + 'px';
     }
 }
+
+function resetFontExample() {
+    let elementsToUpdateFontSize = document.getElementsByClassName('fonts-card-example');
+    for (var i = 0; i < elementsToUpdateFontSize.length; i++) {
+        elementsToUpdateFontSize[i].style.fontSize = "40px";
+    }
+}
